41d347b05d0e5082e7de19f2eba7e8055cce453c
Rémy Sanfeliu <remy.sanfeliu@soprasteria.com>
2019-08-02 21:37:52 +0200

    Initial Commit

---
+++ MoonStone/MoonStone.h
@@ -0,0 +1,19 @@
+//
+//  MoonStone.h
+//  MoonStone
+//
+//  Created by SANFELIU Remy on 02/08/2019.
+//  Copyright © 2019 Rémy Sanfeliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoonStone.
+FOUNDATION_EXPORT double MoonStoneVersionNumber;
+
+//! Project version string for MoonStone.
+FOUNDATION_EXPORT const unsigned char MoonStoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoonStone/PublicHeader.h>
+
+
